b08fda220ed4a2bae72f63acef3d4bfcb83068f4
Doeunnkimm <doeunnkimm@gmail.com>
2023-07-06 15:51:32 +0900

    design: 기본 레이아웃 틀 구성

---
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -0,0 +1,5 @@
+import * as S from './Sidebar.style'
+
+export default function Sidebar() {
+  return <S.Wrapper></S.Wrapper>
+}
---
+++ src/components/Layout/Sidebar/Sidebar.style.js
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components'
+
+export const Wrapper = styled.section`
+  width: 22rem;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.COLOR.sub};
+`
